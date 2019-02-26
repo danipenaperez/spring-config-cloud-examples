69f6d45053ea8c82481b73a4805d9aff009c1a21
Daniel Peña Perez <dpena@paradigmadigital.com>
2019-02-26 15:02:18 +0100

    disabling security to test actuator/refresh

--- spring-cloud-config-server/src/main/java/com/dppware/centralizedConfigServer/CentralizedConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/dppware/centralizedConfigServer/CentralizedConfigServerApplication.java
@@ -3,7 +3,6 @@ package com.dppware.centralizedConfigServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableConfigServer
